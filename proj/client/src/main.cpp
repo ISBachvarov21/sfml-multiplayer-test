4df125f7313f48a004e57c0ab717a3ffa9335289
ISBachvarov21 <ISBachvarov21@codingburgas.bg>
2024-02-11 11:28:19 +0200

    Add game

--- proj/client/src/main.cpp
+++ proj/client/src/main.cpp
@@ -2,38 +2,231 @@
 #include <SFML/Network.hpp>
 #include <stdio.h>
 #include <iostream>
+#include <string>
+#include "../../json/json.hpp"
+
+#define BUFFERSIZE 12000
+
+using json = nlohmann::json;
 
 static sf::UdpSocket socket;
+static std::string addr = "";
+static std::string username = "";
 
-void sendServer(const char* message) {
-    socket.send(message, sizeof(message), sf::IpAddress::getLocalAddress(), 55001);
+void sendServer(const char* message, int retries = 0) {
+    //std::cout << "\033[1;34m[*]Sending message:\n" << message << "\033[0m\n";
+    for (int i = 0; i < retries + 1; ++i) {
+        auto res = socket.send(message, std::strlen(message), sf::IpAddress(addr), 55001);
+        if (res == sf::Socket::Status::Done) {
+            break;
+        }
+    }
 }
 
-void bindSock(uint_fast16_t port) {
-    socket.bind(port, sf::IpAddress::getLocalAddress());
+bool connectToServer(const char* message) {
+    sendServer(message, 5);
 
-    sendServer("connect");
+    //std::cout << "\033[1;34m[*] Sent connect message\033[0m\n";
+    char buffer[4098];
+    memset(buffer, 0, 4098);
+    
+    size_t received;
+    sf::IpAddress respAddr;
+    unsigned short port;
+
+    auto res = socket.receive(buffer, 4098, received, respAddr, port);
+    if (res == sf::Socket::Done) {
+        std::string data(buffer);
+
+        json response = json::parse(data);
+
+        if (response["type"] == "status") {
+            std::cout << "\033[1;32m[+] Connected to server successfully\033[0m\n";
+            return true;
+        }
+        else if (response["type"] == "error") {
+            std::cout << "\033[1;31m[-] Server already full\033[0m\n";
+        }
+    }
+
+    return false;
 }
 
-int main(int argc, char* argv[])
-{
-    uint_fast16_t port = -1;
+void bindSock() {
+    socket.bind(sf::Socket::AnyPort, sf::IpAddress::getLocalAddress());
+    json data;
+
+    data["type"] = "connect";
+    data["username"] = username;
+    if (!connectToServer(data.dump(4).data())) {
+        std::cout << "\033[1;31m[-] Failed to connect to server\033[0m\n";
+
+        std::exception e("Failed to connect to server");
+        throw e;
+    }
+}
+
+int main(int argc, char* argv[]) {
+    std::string addr = "";
+
     for (int i = 0; i < argc; i++) {
-        if (strcmp(argv[i], "--port") == 0) {
-            std::cout << "Found --port" << std::endl;
+        if (strcmp(argv[i], "--addr") == 0) {
             if (i + 1 < argc) {
-                port = (uint_fast16_t)std::stoi(argv[i + 1]);
-                break;
+                addr = std::stoi(argv[i + 1]);
             }
             else {
                 errno = 22;
-                perror("\033[1;31m[-] Invalid --port usage.\n[-] Usage: client --port <port>\033[0m\n");
+                perror("\033[1;31m[-] Invalid --addr usage.\n[-] Usage: client --addr <remoteIPv4Address>");
+                abort();
+            }
+        }
+        else if (strcmp(argv[i], "--username") == 0) {
+            if (i + 1 < argc) {
+                username = argv[i + 1];
+            }
+            else {
+                errno = 22;
+                perror("\033[1;31m[-] Invalid --username usage.\n[-] Usage: client --username <username>");
                 abort();
             }
         }
     }
 
-    bindSock(port);
+    bindSock();
+    
+    socket.setBlocking(false);
+    
+    sf::RenderWindow window(sf::VideoMode(1280, 720), "Test");
+    window.setFramerateLimit(240);
+    
+    char* buffer = new char[BUFFERSIZE];
+    size_t received = 0;
+    sf::IpAddress recvAddr;
+    unsigned short recvPort;
 
+    sf::Vector2f ballPos;
+    sf::CircleShape ball;
+    ball.setFillColor(sf::Color::Red);
+    ball.setRadius(8);
+
+    float playerPos = 0.25;
+    sf::RectangleShape playerPaddle;
+    playerPaddle.setSize(sf::Vector2f(20, 180));
+    playerPaddle.setFillColor(sf::Color::White);
+    playerPaddle.setPosition(sf::Vector2f(0, 180));
+    
+    float opponentPos = 0.25;
+    sf::RectangleShape opponentPaddle;
+    opponentPaddle.setSize(sf::Vector2f(20, 180));
+    opponentPaddle.setFillColor(sf::Color::White);
+    opponentPaddle.setPosition(sf::Vector2f(1260, 180));
+
+    bool leftAligned = false;
+
+    sf::Clock clock;
+    sf::Event ev;
+    float dt;
+    while (window.isOpen()) {
+        memset(buffer, 0, BUFFERSIZE);
+        dt = clock.restart().asSeconds();
+
+        while (window.pollEvent(ev)) {
+            if (ev.type == sf::Event::Closed()) {
+                socket.setBlocking(true);
+
+                json leave;
+
+                leave["type"] = "leave";
+                leave["username"] = username;
+
+                sendServer(leave.dump(4).data(), 5);
+
+                window.close();
+                socket.unbind();
+                break;
+            }
+        }
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::W) && window.hasFocus()) {
+            playerPos += (-1 * 300.f * dt) / 720;
+                    
+            json move;
+
+            move["type"] = "move";
+            move["data"] = playerPos;
+            move["username"] = username;
+
+            sendServer(move.dump(4).data());
+        }
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::S) && window.hasFocus()) {
+            playerPos += (1 * 300.f * dt) / 720;
+
+            json move;
+
+            move["type"] = "move";
+            move["data"] = playerPos;
+            move["username"] = username;
+
+            sendServer(move.dump(4).data());
+        }
+
+        /*sf::Socket::Status res = sf::Socket::Done;
+
+        std::string lastData;
+        while (true) {
+            res = socket.receive(buffer, BUFFERSIZE, received, recvAddr, recvPort);
+            if (res != sf::Socket::Done) {
+                break;
+            }
+            lastData.assign(buffer);
+            memset(buffer, 0, BUFFERSIZE);
+        }*/
+
+        auto res = socket.receive(buffer, BUFFERSIZE, received, recvAddr, recvPort);
+
+        if (res == sf::Socket::Done) {
+            json serverJson;
+            try {
+                serverJson = json::parse(buffer);
+            }
+            catch (json::exception& e) {
+                std::cout << e.what();
+                abort();
+            }
+            
+            std::string alignment = serverJson["players"][username]["alignment"];
+            leftAligned = (alignment == "left");
+
+            ballPos.x = serverJson["ball"]["posX"];
+            ballPos.y = serverJson["ball"]["posY"];
+
+            float serverPos = serverJson["players"][username]["posY"];
+            if (std::abs(playerPos - serverPos) > 0.5f) {
+                playerPos = serverPos;
+            }
+
+            for (auto& [key, value] : serverJson["players"].items()) {
+                if (key != username) {
+                    opponentPos = value["posY"];
+                }
+            }
+        }
+
+        if (leftAligned) {
+            ball.setPosition(ballPos.x * 1280, ballPos.y * 720);
+        }
+        else {
+            ball.setPosition((1 - ballPos.x) * 1280, ballPos.y * 720);
+        }
+        playerPaddle.setPosition(0, playerPos * 720);
+        opponentPaddle.setPosition(1260, opponentPos * 720);
+
+        window.clear();
+        window.draw(ball);
+        window.draw(playerPaddle);
+        window.draw(opponentPaddle);
+        window.display();
+    }
+    
     return 0;
 }
--- proj/server/src/main.cpp
+++ proj/server/src/main.cpp
@@ -1,33 +1,256 @@
-#include <SFML/Graphics.hpp>
 #include <SFML/Network.hpp>
 #include <stdio.h>
 #include <iostream>
+#include <string>
+#include <vector>
+#include "../../json/json.hpp"
+#include <thread>
+#include <mutex>
 
-static sf::UdpSocket socket;
+#define MAX_CLIENTS 2
 
-static void recvClients() {
-    char* buffer = new char[4098];
-    memset(buffer, 0, 4098);
+using json = nlohmann::json;
+
+static sf::UdpSocket clientSocket;
+static sf::UdpSocket gameSocket;
+static json gameState;
+static json clients;
+static uint_least8_t connectedClients = 0;
+static std::mutex mut;
+
+void recvClients() {
+    char buffer[4098];
 
     sf::IpAddress connAddr;
     size_t received;
     unsigned short port;
+    std::string data;
     
     while (true) {
-        socket.receive(buffer, sizeof(buffer), received, connAddr, port);
-        std::cout << "\033[1;34m[*] Received message: " << buffer << "\033[0m\n";
+        memset(buffer, 0, 4098);
+        clientSocket.receive(buffer, 4098, received, connAddr, port);
+        data = std::string(buffer);
+        //std::cout << "\033[1;34m[*] Received message: " << data << "\033[0m\n";
+
+        json response;
+        json dataJson;
+
+        try {
+            dataJson = json::parse(data);
+        }
+        catch (json::exception& e) {
+            std::cout << "\033[1;31m[-] Error while parsing:\n" << e.what() << "\033[0m\n";
+        }
+
+        std::string type;
+        try {
+            type = dataJson["type"];
+        }
+        catch (json::exception& e) {
+            std::cout << "\033[1;31m[-] Error while getting type:\n" << e.what() << "\033[0m\n";
+            abort();
+        }
+        if (type == "connect") {
+            if (connectedClients < MAX_CLIENTS) {
+                response["type"] = "status";
+                response["data"] = "connected";
+                std::string username;
+                
+                try {
+                    username = dataJson["username"];
+                }
+                catch (json::exception& e) {
+                    std::cout << "\033[1;31m[-] Error while parsing username:\n" << e.what() << "\033[0m\n";
+                }
+
+                clients[username]["ip"] = connAddr.toString();
+                clients[username]["port"] = port;
+                
+                mut.lock();
+                gameState["players"][username]["posY"] = 0.25f;
+
+                bool hasLeft = false;
+                if (gameState["players"].size() == 0) {
+                    gameState["players"][username]["alignment"] = "left";
+                }
+                else {
+                    for (auto& [key, value] : gameState["players"].items()) {
+                        if (value["alignment"] == "left") {
+                            hasLeft = true;
+                        }
+                    }
+                    if (hasLeft) {
+                        gameState["players"][username]["alignment"] = "right";
+                    }
+                    else {
+                        gameState["players"][username]["alignment"] = "left";
+                    }
+                }
+                mut.unlock();
+                
+                connectedClients++;
+                for (int i = 0; i < 5; ++i) {
+                    if (clientSocket.send(response.dump().data(), response.dump().size(), connAddr, port) == sf::Socket::Done) {
+                        break;
+                    }
+                }
+            }
+            else {
+                response["type"] = "error";
+                response["data"] = "too many clients";
+                
+                for (int i = 0; i < 5; ++i) {
+                    if (clientSocket.send(response.dump().data(), response.dump().size(), connAddr, port) == sf::Socket::Done) {
+                        break;
+                    }
+                }
+            }
+        }
+        else if (type == "leave") {
+            std::string username;
+
+            try {
+                username = dataJson["username"];
+            }
+            catch (json::exception& e) {
+                std::cout << "\033[1;31m[-] Error while parsing username:\n" << e.what() << "\033[0m\n";
+            }
+            
+            if (clients.contains(username)) {
+                clients.erase(username);
+
+                mut.lock();
+                gameState["players"].erase(username);
+                mut.unlock();
+
+                connectedClients--;
+            }
+        }
+        else if (type == "move") {
+            float y;
+            std::string username;
+
+            try {
+                username = dataJson["username"];
+                y = dataJson["data"];
+            }
+            catch (json::exception& e) {
+                std::cout << "\033[1;31m[-] Error while parsing move data:\n" << e.what() << "\033[0m\n";
+            }
+
+            mut.lock();
+            gameState["players"][username]["posY"] = y;
+            mut.unlock();
+            //for (auto& [key, value] : clients.items()) {
+            //    std::string ip = value["ip"];
+            //    unsigned short port = value["port"];
+            //    mut.lock();
+            //    gameSocket.send(gameState.dump(4).data(), gameState.dump(4).size(), sf::IpAddress(ip), port);
+            //    mut.unlock();
+            //}
+        }
     }
 }
 
 void bindSock() {
-    socket.bind(55001, sf::IpAddress::getLocalAddress());
-
-    recvClients();
+    std::cout << "\033[1;34m[*] Local address: " << sf::IpAddress::getLocalAddress() << "\033[0m\n";
+    clientSocket.bind(55001, sf::IpAddress::getLocalAddress());
+    gameSocket.bind(55002, sf::IpAddress::getLocalAddress());
 }
 
 int main(int argc, char* argv[])
 {
     bindSock();
+
+    bool shouldRun = true;
+    
+    sf::Vector2f simulationSize;
+    simulationSize.x = 1280;
+    simulationSize.y = 1080;
+
+    sf::Vector2f ballPos;
+    ballPos.x = 640;
+    ballPos.y = 360;
+    
+    sf::Vector2i ballDir;
+    ballDir.x = 1;
+    ballDir.y = 1;
+
+    sf::Vector2f player1Pos;
+    player1Pos.x = 0;
+    player1Pos.y = 180;
+    
+    sf::Vector2f player2Pos;
+    player2Pos.x = 1260;
+    player2Pos.y = 180;
+
+    gameState["ball"]["posX"] = 0.5;
+    gameState["ball"]["posY"] = 0.5;
+    
+    std::jthread receiveThread(recvClients);
+
+    sf::Clock clock;
+    float dt;
+    while (shouldRun) {
+        if (clock.getElapsedTime().asSeconds() < 0.016) {
+            continue;
+        }
+
+        if (connectedClients < 2) {
+            clock.restart();
+            continue;
+        }
+
+        dt = clock.getElapsedTime().asSeconds();
+
+
+        ballPos.x += (float)ballDir.x * 400.f * dt;
+        ballPos.y += (float)ballDir.y * 400.f * dt;
+
+        mut.lock();
+        for (auto& [key, value] : gameState["players"].items()) {
+            float posY = value["posY"];
+            if (value["alignment"] == "left") {
+                player1Pos.y = posY * simulationSize.y;
+            }
+            else {
+                player2Pos.y = posY * simulationSize.y;
+            }
+        }
+        mut.unlock();
+
+        if (ballPos.x > 0 && ballPos.x <= player1Pos.x + 20 && ballPos.y + 16 >= player1Pos.y && ballPos.y <= player1Pos.y + 260 && ballDir.x == -1) {
+            ballDir.x *= -1;
+        }
+        else if (ballPos.x + 8 < 1280 && ballPos.x + 8 >= player2Pos.x && ballPos.y + 16 >= player2Pos.y && ballPos.y <= player2Pos.y + 260 && ballDir.x == 1) {
+            ballDir.x *= -1;
+        }
+        if ((ballPos.x <= 0 && ballDir.x == -1) || (ballPos.x + 8 >= simulationSize.x && ballDir.x == 1)) {
+            ballPos = sf::Vector2f(640 - 16, 360 - 16);
+            ballDir *= -1;
+        }
+        if ((ballPos.y <= 0 && ballDir.y == -1) || (ballPos.y + 16 >= simulationSize.y && ballDir.y == 1)) {
+            ballDir.y *= -1;
+        }
+
+        mut.lock();
+        gameState["ball"]["posX"] = ballPos.x / simulationSize.x;
+        gameState["ball"]["posY"] = ballPos.y / simulationSize.y;
+        mut.unlock();
+
+        for (auto& [key, value] : clients.items()) {
+            std::string ip = value["ip"];
+            unsigned short port = value["port"];
+            mut.lock();
+            gameSocket.send(gameState.dump(4).data(), gameState.dump(4).size(), sf::IpAddress(ip), port);
+            mut.unlock();
+        }
+
+        clock.restart();
+    }
+
+    clientSocket.unbind();
+    gameSocket.unbind();
 
     return 0;
 }
